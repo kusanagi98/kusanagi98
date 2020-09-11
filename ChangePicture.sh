1cdbc531348e87faf468c2696cf9a99b5cb8dc79
Bach Do <36164826+kusanagi98@users.noreply.github.com>
2020-09-11 08:25:28 +0700

    Update ChangePicture.sh

--- ChangePicture.sh
+++ ChangePicture.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-imgUrl=("mkSi8UG", "6dUE06q", "MzU1b3q") 
+imgUrl=("mkSi8UG.png", "6dUE06q.png", "MzU1b3q.png") 
 RANDOM=$$
 R=`expr ${RANDOM} \% 3`
 rm README.md 
-echo "![WEEBOO](https://i.imgur.com/${imgUrl}.png)" >> README.md
+echo "![WEEBOO](https://i.imgur.com/${imgUrl})" >> README.md
